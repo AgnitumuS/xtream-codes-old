571c4c75dabd8fe771d85b86c6588ffb84ad2fd5
davigon <dvgnlz@outlook.es>
2022-06-05 17:53:09 +0200

    Add react query

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -6,21 +6,26 @@ import { Navbar } from "./components/Navbar"
 import { HomePage } from "./pages/HomePage"
 import { NotFoundPage } from "./pages/NotFoundPage"
 import theme from "./theme"
+import { QueryClient, QueryClientProvider } from "react-query"
+
+const queryClient = new QueryClient()
 
 export const App = () => {
   return (
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <div className="App">
-        <Navbar />
-        <main className="Main">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <div className="App">
+          <Navbar />
+          <main className="Main">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </QueryClientProvider>
     </ChakraProvider>
   )
 }
