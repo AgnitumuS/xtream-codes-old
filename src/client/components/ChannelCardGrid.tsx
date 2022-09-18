686844a33e66969852b65bb00c048f130043c04b
davigon <davigon.personal@outlook.es>
2022-09-19 01:28:50 +0200

    Add channel grid pagination

--- src/client/components/ChannelCardGrid.tsx
+++ src/client/components/ChannelCardGrid.tsx
@@ -1,7 +1,9 @@
 import React from "react"
-import { Grid } from "@chakra-ui/react"
+import { Center, Grid, Spinner } from "@chakra-ui/react"
 import { Channel } from "../../../types/types"
 import { ChannelCard } from "./ChannelCard"
+import InfiniteScroll from "react-infinite-scroll-component"
+import { useScrollConstData } from "../hooks/useScrollConstData"
 
 type ChannelCardGridProps = {
   channels: Channel[]
@@ -9,12 +11,26 @@ type ChannelCardGridProps = {
 
 export const ChannelCardGrid = (props: ChannelCardGridProps) => {
   const { channels } = props
+  const { data, currentDataLength, hasMore, loadNextData } =
+    useScrollConstData(channels)
 
   return (
-    <Grid templateColumns="repeat(auto-fill, minmax(12rem, 1fr))" gap={4}>
-      {channels.map((key) => {
-        return <ChannelCard key={key.stream_id} channel={key} />
-      })}
-    </Grid>
+    <InfiniteScroll
+      style={{ overflow: "hidden" }}
+      dataLength={currentDataLength}
+      hasMore={hasMore}
+      loader={
+        <Center>
+          <Spinner thickness="4px" speed="0.65s" color="blue.500" size="lg" />
+        </Center>
+      }
+      next={loadNextData}
+    >
+      <Grid templateColumns="repeat(auto-fill, minmax(12rem, 1fr))" gap={4}>
+        {data.map((key) => {
+          return <ChannelCard key={key.stream_id} channel={key} />
+        })}
+      </Grid>
+    </InfiniteScroll>
   )
 }
